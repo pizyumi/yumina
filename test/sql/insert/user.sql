95ca36612a7348ca773fbae38a50b68135536ba1
pizyumi <piz_yumina@hotmail.com>
2018-06-24 00:44:20 +0900

    add test sql

---
+++ test/sql/insert/user.sql
@@ -0,0 +1,3 @@
+insert into t_user values (1, 'yume', 'ippai', null);
+insert into t_user values (2, 'yurina', 'kawaii', '2018-05-31T12:21:03.123Z');
+insert into t_user values (3, 'yumina', 'mtftx', null);
