6430f7ad9b8fe44b778aaf1326a29c7f91616f7d
pizyumi <piz_yumina@hotmail.com>
2018-07-10 01:21:14 +0900

    fix user dao

--- sample/entity/user/dao.js
+++ sample/entity/user/dao.js
@@ -1,4 +1,4 @@
-module.exports = (funcs, dao, db, common) => {
+module.exports = (d, db, common) => {
   return {
     user_by_name: async (name) => {
       return await common.select_one_by_sql(db, 'select * from t_user where name=@name and delete_date is null', { name });
