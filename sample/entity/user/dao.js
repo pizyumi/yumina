a8132b9037c3a102dd80799eadbd3eef285669a4
pizyumi <piz_yumina@hotmail.com>
2018-07-10 00:44:17 +0900

    add user dao

---
+++ sample/entity/user/dao.js
@@ -0,0 +1,7 @@
+module.exports = (funcs, dao, db, common) => {
+  return {
+    user_by_name: async (name) => {
+      return await common.select_one_by_sql(db, 'select * from t_user where name=@name and delete_date is null', { name });
+    }
+  }
+};
