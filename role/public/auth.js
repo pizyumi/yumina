15d9c77dd2452ad7a873fa3ab6d2dc8e65879d34
pizyumi <piz_yumina@hotmail.com>
2018-07-17 14:33:19 +0900

    can omit role folder

---
+++ role/public/auth.js
@@ -0,0 +1,36 @@
+var _ = require('underscore');
+
+module.exports = (common) => {
+  return {
+    auth_api: (req, res, next) => {
+      next();
+    },
+    auth_web: (req, res, next) => {
+      next();
+    },
+    list_id: async (req, entity, e, scommon) => {
+      return {};
+    },
+    item_id: async (req, entity, e, scommon) => {
+      var keys = await e.get_table_key_column_names(entity);
+
+      return _.extend({}, _.object(keys, scommon.handle_ids_parameter(req)));
+    },
+    new_id: async (req, entity, e, scommon) => {
+      return {};
+    },
+    update_id: async (req, entity, e, scommon) => {
+      var keys = await e.get_table_key_column_names(entity);
+
+      return _.extend({}, _.object(keys, scommon.handle_ids_parameter(req)));
+    },
+    delete_id: async (req, entity, e, scommon) => {
+      var keys = await e.get_table_key_column_names(entity);
+
+      return _.extend({}, _.object(keys, scommon.handle_ids_parameter(req)));
+    },
+    get_title: (req, con) => {
+      return con.appname;
+    }
+  }
+};
